8afae1f7e13d739d0629374fdce1e23c101a6693
Wladimir <weizen.w@gmx.de>
2023-05-03 15:37:33 +0200

    feat: add class Soda

--- src/Soda.java
+++ src/Soda.java
@@ -1,3 +1,16 @@
 public class Soda {
 
+  private String additive;
+
+  public Soda(String additive) {
+    this.additive = additive;
+  }
+
+  public String getAdditive() {
+    return additive;
+  }
+
+  public void setAdditive(String additive) {
+    this.additive = additive;
+  }
 }
